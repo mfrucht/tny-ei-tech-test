a10dbca5f85e7294ca035de603497956cffecbab
Matthew Frucht <msf325@gmail.com>
2023-09-13 15:19:08 -0400

    Updates database schema and imports newly structured data

---
+++ server/db/tables/authors.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `authors` (
+	`author_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name`	TEXT,
+	`date_of_birth`	TEXT,
+	`date_of_death`	TEXT
+);
+INSERT INTO `authors` VALUES (1,'David Foster Wallave','1962-02-21','2008-09-12');
+INSERT INTO `authors` VALUES (2,'Zadie Smith','1975-10-25',NULL);
+INSERT INTO `authors` VALUES (3,'Vladimir Nabokov','1899-04-22','1977-07-02');
---
+++ server/db/tables/publishers.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `publishers` (
+	`publisher_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`publisher_name`	INTEGER,
+	`city`	TEXT
+);
+INSERT INTO `publishers` VALUES (1,'Vintage','New York');
+INSERT INTO `publishers` VALUES (2,'Penguin','London');
+INSERT INTO `publishers` VALUES (3,'Viking','New York');
+INSERT INTO `publishers` VALUES (4,'Back Bay Books','Boston');
+INSERT INTO `publishers` VALUES (5,'Abacus','New York');
+INSERT INTO `publishers` VALUES (6,'Little, Brown and Company','New york');
+COMMIT;

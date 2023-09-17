64ad040781a33448da5ac785fee17a427483f040
Matthew Frucht <msf325@gmail.com>
2023-09-17 11:45:30 -0400

    Implements search

--- client/src/components/BookListItem.js
+++ client/src/components/BookListItem.js
@@ -9,11 +9,11 @@ const Book = ({
 }) => {
   return (
     <>
-      <a href={'/books/'+isbn} class="block hover:bg-gray-200 p-4 transition-all border-b border-emerald-500 ">
+      <a href={'/books/'+isbn} class="block hover:bg-gray-200 pt-4 pb-2 transition-all border-b border-emerald-500 ">
       <li key={isbn} class="mb-4">
         <h2 class="text-lg font-bold">{title}</h2>
         <div class="flex justify-between">
-          <p class="mt-1 mr-4">Author: {authorName}</p>
+          <p class="mt-1 mr-4">{authorName}</p>
           <p class="mt-1">{year}</p>
         </div>
       </li>
--- client/src/components/pages/BookList.js
+++ client/src/components/pages/BookList.js
@@ -3,6 +3,18 @@ import Book from '../BookListItem';
 
 export default function Books() {
   const [books, setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    const filtered = books.filter((book) =>
+      book.title.toLowerCase().includes(query.toLowerCase()) ||
+      book.authorName.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredBooks(filtered);
+  };
 
   useEffect(() => {
     fetch('http://localhost:3001/books')
@@ -15,12 +27,32 @@ export default function Books() {
   return (
     <>
       <h1 class='text-3xl mb-4 pt-5 text-center'>Books</h1>
-      {books? books.map((book) => {
-        return <ul role="list" class="pl-10 pr-10 divide-y divide-slate-200">
-          <Book key={book.id} {...book} />
-        </ul>
-        })
-        : null }
+      <input
+        type="text"
+        placeholder="Search by title, author"
+        value={searchQuery}
+        onChange={handleSearch}
+        class="bg-white border-2 border-gray-300 rounded-lg p-2 ml-10 mb-4"
+      />
+      <ul>
+        {books ? (
+          searchQuery === '' ? (
+            books.map((book) => (
+              <ul role="list" class="pl-10 pr-10 divide-y divide-slate-200">
+                <Book key={book.id} {...book} />
+              </ul>
+            ))
+          ) : (
+            filteredBooks.map((book) => (
+              <ul role="list" class="pl-10 pr-10 divide-y divide-slate-200">
+                <Book key={book.id} {...book} />
+              </ul>
+            ))
+          )
+        ) : (
+          <p>No books found.</p>
+        )}
+      </ul>
     </>
   );
 }

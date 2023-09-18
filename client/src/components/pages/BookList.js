d7049f67cae72d73f06dc5f82dcdf7549ecff3e2
Matthew Frucht <msf325@gmail.com>
2023-09-17 23:34:02 -0400

    Adds front end form for adding book

--- client/src/components/pages/BookList.js
+++ client/src/components/pages/BookList.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
 import Book from '../BookListItem';
+import AddBookForm from '../AddBookForm';
 
 export default function Books() {
   const [books, setBooks] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredBooks, setFilteredBooks] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
 
   const handleSearch = (event) => {
     const query = event.target.value;
@@ -16,6 +22,29 @@ export default function Books() {
     setFilteredBooks(filtered);
   };
 
+  const handleAddBook = (newBook) => {
+    fetch('/book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBook),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error adding book to database');
+        }
+      })
+      .then((data) => {
+        console.log(data); 
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   useEffect(() => {
     fetch('http://localhost:3001/books')
     .then(response => response.json())
@@ -25,15 +54,27 @@ export default function Books() {
   }, []);
 
   return (
-    <>
+    <div>
       <h1 class='text-3xl mb-4 pt-5 text-center'>Books</h1>
-      <input
-        type="text"
-        placeholder="Search by title, author"
-        value={searchQuery}
-        onChange={handleSearch}
-        class="bg-white border-2 border-gray-300 rounded-lg p-2 ml-10 mb-4"
-      />
+      <div class="flex justify-between">
+        <input
+          type="text"
+          placeholder="Search by title, author"
+          value={searchQuery}
+          onChange={handleSearch}
+          class="bg-white border-2 border-gray-300 rounded-lg p-2 ml-10 mb-4"
+        />
+        <button
+          onClick={togglePopup}
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 mb-4"
+        >Add Book</button>
+      </div>
+      
+      {showPopup && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <AddBookForm onClose={togglePopup} />
+        </div>
+      )}
       <ul>
         {books ? (
           searchQuery === '' ? (
@@ -53,6 +94,6 @@ export default function Books() {
           <p>No books found.</p>
         )}
       </ul>
-    </>
+    </div>
   );
 }
